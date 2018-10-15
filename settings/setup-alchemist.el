9de2f9b6c8b4b74c84ccbbdd2dc58b00d8de3e6a
Trexnix <trexnix@gmail.com>
2018-10-15 12:14:39 +0700

    Add hook function to format Elixir code on save

--- settings/setup-alchemist.el
+++ settings/setup-alchemist.el
@@ -1,3 +1,7 @@
 (require 'alchemist)
 
+;; Format Elixir code on save
+(add-hook 'elixir-mode-hook
+          (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
+
 (provide 'setup-alchemist)
