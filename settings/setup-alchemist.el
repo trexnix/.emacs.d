634900dabe72c837cf024724ae2eb099b85d113f
Trexnix <trexnix@gmail.com>
2019-06-15 23:21:14 +0700

    Workaround fix for Mix format on saving problem

--- settings/setup-alchemist.el
+++ settings/setup-alchemist.el
@@ -4,4 +4,10 @@
 (add-hook 'elixir-mode-hook
           (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
 
+;; Workaround fix for Mix Format on save problem
+;; https://github.com/asdf-vm/asdf-elixir/issues/62
+(eval-after-load "elixir-mode"
+  '(defun elixir-format--mix-executable ()
+     (string-trim-right (shell-command-to-string "asdf which mix"))))
+
 (provide 'setup-alchemist)
